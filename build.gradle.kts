220cf3bd700a76e62ce6a3b6a7d4d67b097dadca
nasc1mento <anr@discente.ifpe.edu.br>
2025-06-09 01:20:42 -0300

    Deleting unused dependency: SPIRE Doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,9 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://repo.e-iceblue.com/nexus/content/groups/public/")
-    }
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/e-iceblue/spire.doc.free
-    implementation("e-iceblue:spire.doc.free:5.3.2")
-
     // https://mvnrepository.com/artifact/org.apache.poi/poi
     implementation("org.apache.poi:poi:5.4.1")
     // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
